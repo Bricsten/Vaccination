7dee31dacb56a166bf111a6ad4c07578225797ce
Bricsten <bricstenbello@gmail.com>
2024-06-21 20:22:06 +0100

    Created footer and header

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Footer() {
-    return (
-        <div>Footer</div>
-    )
-}
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-light py-4">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <p>&copy; 2024 Polio Vaccination Monitoring System. All rights reserved.</p>
+          </div>
+          <div className="col-md-6 text-md-right">
+            <p>
+              Contact Us:
+              <a href="mailto:info@poliovaccine.com" className="text-light ml-2">
+                info@poliovaccine.com
+              </a>
+            </p>
+            <p>
+              Phone: <a href="tel:+1234567890" className="text-light">+1 (234) 567-890</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
