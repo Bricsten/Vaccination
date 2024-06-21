590741ae7140d822a6b78c7d3a9255b709647d30
Bricsten <bricstenbello@gmail.com>
2024-06-21 16:48:35 +0100

    added the footer i noticed u already added header

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< HEAD
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Footer = () => {
@@ -22,6 +23,33 @@ const Footer = () => {
           </div>
         </div>
       </div>
+=======
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-white py-4">
+      <Container>
+        <Row className="align-items-center">
+          <Col md={6} className="mb-3 mb-md-0">
+            <p className="mb-0">&copy; 2023 Polio Vaccination Monitoring System. All rights reserved.</p>
+          </Col>
+          <Col md={6} className="text-md-right">
+            <ul className="list-inline mb-0">
+              <li className="list-inline-item mr-3">
+                <a href="#" className="text-white">Privacy Policy</a>
+              </li>
+              <li className="list-inline-item mr-3">
+                <a href="#" className="text-white">Terms of Service</a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#" className="text-white">Contact Us</a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+>>>>>>> 2fd0e27 (added the footer i noticed u already added header)
     </footer>
   );
 };
