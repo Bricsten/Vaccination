a71f995520bb1935e481beb563f1b842cb729f1e
percyarrey <tanyitikuarrey@gmail.com>
2024-06-21 20:54:04 +0100

    working on login page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -32,9 +32,13 @@ const router = createBrowserRouter([
 ]);
 root.render(
   <React.StrictMode>
-    <Header />
-    <RouterProvider router={router} />
-    <Footer />
+    <div className='d-flex flex-column justify-content-between' style={{ height: '100vh' }}>
+      <div className='h-100 flex-1'>
+        <Header />
+        <RouterProvider router={router} />
+      </div>
+      <Footer />
+    </div>
   </React.StrictMode>
 );
 
